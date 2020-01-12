738003f2719209ac63d952b44a2c64e3a421046e
Christopher Ejiofor <cejiofor@gmail.com>
2020-01-12 17:34:43 -0600

    complete simple solution

--- ComparatorChecker.java
+++ ComparatorChecker.java
@@ -17,8 +17,26 @@ class ComparatorChecker implements Comparator<Player> {
                 return -1;
             }
             else{
-                return 0;
+                return compareString(a.name, b.name);
             } 
         }
     }
+
+    static int compareString(String a, String b){
+        char aFirstLetter = a.charAt(0);
+        char bFirstLetter = b.charAt(0);
+        if (aFirstLetter > bFirstLetter){
+            return 1;
+        }
+        else if (aFirstLetter < bFirstLetter){
+            return -1;
+        }
+        else if (a.length() == 1 || b.length() ==1){
+            return 0;
+        }
+        else{
+            return compareString(a.substring(1), b.substring(2));
+        }
+    }    
+
 }
